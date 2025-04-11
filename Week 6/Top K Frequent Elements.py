eaf52f49cfa9cca66fcfd9ef993414ff1ee6d2fb
saiharshavarma <saiharshavarma@outlook.com>
2025-04-11 12:22:24 -0400

    week 6 solved

---
+++ Week 6/Top K Frequent Elements.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        freq = [[] for _ in range(len(nums) + 1)]
+        
+        for num, c in count.items():
+            freq[c].append(num)
+        
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
