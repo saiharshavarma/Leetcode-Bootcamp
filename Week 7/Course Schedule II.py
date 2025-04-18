b84cac4b0acafe36a1e055587790794b795c0ab1
saiharshavarma <saiharshavarma@outlook.com>
2025-04-18 16:01:30 -0400

    week 7 solved

---
+++ Week 7/Course Schedule II.py
@@ -0,0 +1,23 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+            in_degree[a] += 1
+
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        order = []
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            for neighbor in graph[course]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+
+        return order if len(order) == numCourses else []
